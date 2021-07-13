95e65fa5ee9ab67dc59b901e597a2c86d88d6850
Adilson de Brito <abrito10@gmail.com>
2021-07-13 09:39:23 -0300

    first commit

---
+++ ponto/src/test/java/br/com/abrito/ponto/PontoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.abrito.ponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
